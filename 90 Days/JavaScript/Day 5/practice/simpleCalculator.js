f4bb7ba992b32a2e45baab23313ecdfecd088c2d
NaviTalib <talibali786.tagmail.com>
2026-01-26 15:53:53 +0530

    JS basic practice done

---
+++ 90 Days/JavaScript/Day 5/practice/simpleCalculator.js
@@ -0,0 +1,9 @@
+// console base calculator
+
+let a = 6;
+let b = 3;
+
+console.log("Addition: " + (a + b)); // Addition: 9
+console.log("Subtraction: " + (a - b)); // Subtraction: 3
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b)); // Division: 2
---
+++ 90 Days/JavaScript/Day 5/practice/swap2Variables.js
@@ -0,0 +1,20 @@
+let a = 6;
+let b = 9;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log("a:", a);
+// console.log("b:", b);
+
+// [a, b] = [b, a];
+// console.log("a:", a);
+// console.log("b:", b);
+
+a = a + b; // a = 15
+b = a - b; // b = 6
+a = a - b; // a = 9
+
+console.log("a:", a);
+console.log("b:", b);
