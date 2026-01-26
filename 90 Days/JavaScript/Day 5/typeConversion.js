d10a857948851e44c4b4f33ca0da6133bc9a9e4b
NaviTalib <talibali786.tagmail.com>
2026-01-26 15:42:10 +0530

    type conversion done

---
+++ 90 Days/JavaScript/Day 5/typeConversion.js
@@ -0,0 +1,12 @@
+let a = 55;console.log(typeof a); // number
+let b = String(a);
+console.log(typeof b); // string
+
+let c = Boolean(a);
+console.log(typeof c); // boolean
+
+console.log("5"+5);
+console.log("5"-5);
+console.log("5"*5);
+console.log("5"/5);
+console.log("5"%2);
