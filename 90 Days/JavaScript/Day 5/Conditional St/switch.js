b61b363123a0c058be9221430fef1babcd17f471
NaviTalib <talibali786.tagmail.com>
2026-01-26 16:52:54 +0530

    conditional stmnt done

---
+++ 90 Days/JavaScript/Day 5/Conditional St/switch.js
@@ -0,0 +1,16 @@
+let marks = 60;
+
+switch (marks) {
+    case 90:
+        console.log("Grade A");
+        break;
+    case 75:
+        console.log("Grade B");
+        break;
+    case 60:
+        console.log("Grade C");
+        break;
+    default:
+        console.log("Grade D");
+}
+
