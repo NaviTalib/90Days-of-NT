55e1611e5cb705cdc133cf1e1b2bb46e2d052f61
NaviTalib <talibali786.tagmail.com>
2026-01-28 13:37:46 +0530

    Arrays basics done

---
+++ 90 Days/JavaScript/Day 7/Arr.js
@@ -0,0 +1,40 @@
+let numbers = [10,20,30,40,50];
+
+// let names = ["a","b","c","d","e"];
+
+// let mixed = [10, "hello", true, 20, "world", false];
+
+// console.log("Numbers Array:", numbers);
+// console.log("Names Array:", names);
+// console.log("Mixed Array:", mixed);
+
+
+// Accessing elements by index
+// console.log(numbers[0]); // 10
+
+// Modifying elements
+numbers[1] = 25;
+// console.log(numbers); 
+
+// console.log("Length of numbers array:", numbers.length);
+
+numbers.push(60); // Adding an element at the end
+
+numbers.pop(); // Removing the last element
+
+numbers.unshift(5); // Adding an element at the beginning
+numbers.shift(); // Removing the first element
+
+// console.log(numbers);
+
+
+// looping through the array
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log("Element at index", i, "is", numbers[i]);
+// }
+
+// for (let num of numbers) {
+//     console.log("Number:", num);
+// }
+
