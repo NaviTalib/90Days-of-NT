dc44ab2e046fc7aa15b6fccc3ac6bd25ba593cd9
NaviTalib <talibali786.tagmail.com>
2026-01-31 00:22:51 +0530

    objects destructuring done

--- 90 Days/JavaScript/Day 9/destruct.js
+++ 90 Days/JavaScript/Day 9/destruct.js
@@ -1,12 +1,33 @@
 // Destructuring lets you extract values from arrays or objects into variable in one line
 
-let username = {
+let user = {
     name : "navicodes",
     age : 22,
     country : "India"
 }
 
-let {name, age, country} = username;
+// let {name, age, country} = user;
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
+
+// rename while destructuring
+
+// let {name : userName, city : userCity} = user;
+
+// console.log(userName)
+
+
+//  default value 
+
+// let { country = "india"} = user;
+
+// console.log(country)
+
+
+// destructuring in function parameters
+
+// function greet({name, age}){
+//     console.log(`Hi ${name} age ${age}`);
+// }
+// greet(user);
