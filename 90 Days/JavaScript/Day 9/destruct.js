f408091bfbfc701f3c1bdc456b1826cb1dc40f3c
NaviTalib <talibali786.tagmail.com>
2026-01-30 23:28:06 +0530

    day 9 start with destructuring

---
+++ 90 Days/JavaScript/Day 9/destruct.js
@@ -0,0 +1,12 @@
+// Destructuring lets you extract values from arrays or objects into variable in one line
+
+let username = {
+    name : "navicodes",
+    age : 22,
+    country : "India"
+}
+
+let {name, age, country} = username;
+
+console.log(name);
+console.log(age);
