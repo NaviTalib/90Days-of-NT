8c957bcc914c2fabeaa3f325fb3639d9e74953cb
NaviTalib <talibali786.tagmail.com>
2026-01-31 14:58:51 +0530

    destructuring done

--- 90 Days/JavaScript/Day 9/destruct1.js
+++ 90 Days/JavaScript/Day 9/destruct1.js
@@ -9,3 +9,13 @@ let [first,second] = colors;
 // let [, , third] = colors;
 
 // console.log(third)
+
+// swap two numbers 
+
+// let a = 7, b = 66;
+
+// [a,b] = [b,a];
+
+// console.log("a",a);
+// console.log("b",b);
+
